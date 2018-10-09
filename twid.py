35f71f792e8226069d462531ed34288e96b6e049
Adam Mukuddem <shortmuki@gmail.com>
2018-10-09 22:18:10 +0200

    initialization

---
+++ twid.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import Adafruit_MCP3008
+import time
+import os
+import spidev
+import sys
+import subprocess as sp
+
+GPIO.setmode(GPIO.BCM)
+
+#pin Definition
+
+SPICLK = 11
+SPIMISO = 9 
+SPIMOSI = 10
+SPICS = 8
+
+# pin numbers switch
+
+start = 19
+
+#SET ADC Pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+#Button pin setups
+GPIO.setup(start, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+
+mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK,   cs=SPICS,   mosi=SPIMOSI,  miso=SPIMISO)
