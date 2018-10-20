1e19e9e598fdcbc9306a8f9cef88eaf23d69a622
Adam Mukuddem <shortmuki@gmail.com>
2018-10-20 11:15:10 +0200

    Update twid.py

--- twid.py
+++ twid.py
@@ -42,7 +42,7 @@ mcp = Adafruit_MCP3008.MCP3008(clk=SPICLK,   cs=SPICS,   mosi=SPIMOSI,  miso=SPI
 #Global Variables
 dur = [9]*16;
 code =[1,1,0];
-times = [2000,2000,2000,1000]
+times = [2000,2000,2000]
 dir = [9]*16;
 values = [0]*8
 count = 0;
