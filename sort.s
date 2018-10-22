f562382b401b0ce4538d798f8d5e33cb951a4dce
Kavishc <chauhankavish@gmail.com>
2018-10-22 12:01:58 +0200

    testing

--- sort.s
+++ sort.s
@@ -1,6 +1,18 @@
 /* Sorting function in assembly */
 
+
+/* -----------------Memory Section-------------------*/
+
 .data
 a: .skip 64 /*assign 64 reserve 64 bytes of data for 16 entries*/
 
- 
+/* --------------------------------------------------*/
+
+/*----------------Code Section ---------------------*/
+.text
+.global main
+main:
+	mov r0, #2
+	bx lr
+
+/*---------------------------------------------------*/
